8e272867993e306f988ebc9e371170502c76b5f8
Fatih Erikli <fatiherikli@gmail.com>
2012-05-30 13:54:25 +0300

    Function.bind extension check for browser support

--- src/bind.js
+++ src/bind.js
@@ -1,10 +1,14 @@
-
-    Function.prototype.bind = function(obj) { 
-        var method = this;
     
-        tmp = function() {
-            return method.apply(obj, arguments);
+    if (typeof Function.bind === 'undefined') {
+    
+        Function.prototype.bind = function(obj) { 
+            var method = this;
+        
+            tmp = function() {
+                return method.apply(obj, arguments);
+            };
+        
+            return tmp;
         };
     
-        return tmp;
-    };
+    }
